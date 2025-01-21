b89e2a9cf238d09a95f3e824270c3657ff8b8ca1
Kaden Chow <kaden.chow@gmail.com>
2025-01-21 02:07:29 -0800

    1/21 - Updated All April Tags

--- src/main/java/frc/robot/FieldSetup.java
+++ src/main/java/frc/robot/FieldSetup.java
@@ -11,24 +11,33 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class FieldSetup {
-        public static final Translation2d bluePassPosition = new Translation2d(0.6, 7.5);
-        public static final Translation2d redPassPosition = new Translation2d(16, 7.5);
+        // public static final Translation2d bluePassPosition = new Translation2d(0.6, 7.5);
+        // public static final Translation2d redPassPosition = new Translation2d(16, 7.5);
+        public static final Translation2d blueBargeAPosition = new Translation2d(8.775, 7.270);
+        public static final Translation2d blueBargeBPosition = new Translation2d(8.775, 6.175);
+        public static final Translation2d blueBargeCPosition = new Translation2d(8.775, 5.075);
+        public static final Translation2d RedBargeAPosition = new Translation2d(8.775, 3.000);
+        public static final Translation2d RedBargeBPosition = new Translation2d(8.775, 1.905);
+        public static final Translation2d RedBargeCPosition = new Translation2d(8.775, 0.810);
 
-        public static final Supplier<Translation2d> alliancePassPositionSupplier = () -> DriverStation.getAlliance()
-                .orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Blue
-                        ? bluePassPosition
-                        : redPassPosition;
+        // public static final Supplier<Translation2d> alliancePassPositionSupplier = () -> DriverStation.getAlliance()
+        //         .orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Blue
+        //                 ? bluePassPosition
+        //                 : redPassPosition;
 
         public static final TreeMap<Integer, Translation2d> kRedAprilTagLayout = new TreeMap<Integer, Translation2d>() {
             {
-                put(1, new Translation2d(Units.inchesToMeters(593.68), Units.inchesToMeters(9.68)));
-                put(2, new Translation2d(Units.inchesToMeters(637.21), Units.inchesToMeters(34.79)));
-                put(3, new Translation2d(Units.inchesToMeters(652.73), Units.inchesToMeters(196.17)));
-                put(4, new Translation2d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42)));
-                put(5, new Translation2d(Units.inchesToMeters(578.77), Units.inchesToMeters(323.00)));
-                put(11, new Translation2d(Units.inchesToMeters(468.69), Units.inchesToMeters(146.19)));
-                put(12, new Translation2d(Units.inchesToMeters(468.69), Units.inchesToMeters(177.10)));
-                put(13, new Translation2d(Units.inchesToMeters(441.74), Units.inchesToMeters(161.62)));
+                put(12, new Translation2d(Units.inchesToMeters(33.51),Units.inchesToMeters(25.80)));
+                put(13, new Translation2d(Units.inchesToMeters(33.51),Units.inchesToMeters(291.20)));
+                put(14, new Translation2d(Units.inchesToMeters(325.68),Units.inchesToMeters(241.64)));
+                put(15, new Translation2d(Units.inchesToMeters(325.68),Units.inchesToMeters(75.39)));
+                put(16, new Translation2d(Units.inchesToMeters(235.73),Units.inchesToMeters(-0.15)));
+                put(17, new Translation2d(Units.inchesToMeters(160.39),Units.inchesToMeters(130.17)));
+                put(18, new Translation2d(Units.inchesToMeters(144.00),Units.inchesToMeters(158.50)));
+                put(19, new Translation2d(Units.inchesToMeters(160.39),Units.inchesToMeters(186.83)));
+                put(20, new Translation2d(Units.inchesToMeters(193.10),Units.inchesToMeters(186.83)));
+                put(21, new Translation2d(Units.inchesToMeters(209.49),Units.inchesToMeters(158.50)));
+                put(22, new Translation2d(Units.inchesToMeters(193.10),Units.inchesToMeters(130.17)));
                 put(51, new Translation2d(Units.inchesToMeters(566.77), Units.inchesToMeters(287.00)));
             }
 
@@ -36,17 +45,22 @@ public class FieldSetup {
 
         public static final TreeMap<Integer, Translation2d> kBlueAprilTagLayout = new TreeMap<Integer, Translation2d>() {
             {
-                put(6, new Translation2d(Units.inchesToMeters(72.5), Units.inchesToMeters(323.00)));
-                put(7, new Translation2d(Units.inchesToMeters(-1.50), Units.inchesToMeters(218.42)));
-                put(8, new Translation2d(Units.inchesToMeters(-1.50), Units.inchesToMeters(196.17)));
-                put(9, new Translation2d(Units.inchesToMeters(14.02), Units.inchesToMeters(34.79)));
-                put(10, new Translation2d(Units.inchesToMeters(57.54), Units.inchesToMeters(9.68)));
-                put(14, new Translation2d(Units.inchesToMeters(209.48), Units.inchesToMeters(161.62)));
-                put(15, new Translation2d(Units.inchesToMeters(182.73), Units.inchesToMeters(177.10)));
-                put(16, new Translation2d(Units.inchesToMeters(182.73), Units.inchesToMeters(146.19)));
+                put(1, new Translation2d(Units.inchesToMeters(657.37),Units.inchesToMeters(25.80)));
+                put(2, new Translation2d(Units.inchesToMeters(657.37),Units.inchesToMeters(291.20)));
+                put(3, new Translation2d(Units.inchesToMeters(455.15),Units.inchesToMeters(317.15)));
+                put(4, new Translation2d(Units.inchesToMeters(365.20),Units.inchesToMeters(241.64)));
+                put(5, new Translation2d(Units.inchesToMeters(365.20),Units.inchesToMeters(75.39)));
+                put(6, new Translation2d(Units.inchesToMeters(530.49),Units.inchesToMeters(130.17)));
+                put(7, new Translation2d(Units.inchesToMeters(546.87),Units.inchesToMeters(158.50)));
+                put(8, new Translation2d(Units.inchesToMeters(530.49),Units.inchesToMeters(186.83)));
+                put(9, new Translation2d(Units.inchesToMeters(497.77),Units.inchesToMeters(186.83)));
+                put(10, new Translation2d(Units.inchesToMeters(481.39),Units.inchesToMeters(150.50)));
+                put(11, new Translation2d(Units.inchesToMeters(497.77),Units.inchesToMeters(130.17)));
                 put(61, new Translation2d(Units.inchesToMeters(0), Units.inchesToMeters(287)));
             }
         };
+
+        //put(, new Translation2d(Units.inchesToMeters(X),Units.inchesToMeters(Y)));
 
         public static final int kRedSpeakerId = 4;
         public static final int kBlueSpeakerId = 7;
